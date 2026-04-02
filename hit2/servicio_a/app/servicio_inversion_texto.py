89c339fe99f5a87680a895d5925ab567cc1308c2
GustavoContardi <lito.contardi@hotmail.com>
2026-04-02 19:54:29 -0300

    Hit2_v1

---
+++ hit2/servicio_a/app/servicio_inversion_texto.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",    
+    handlers=[logging.StreamHandler(sys.stdout)]
+    )
+
+logger = logging.getLogger("ServicioInvertirTexto")
+
+
+app = Flask(__name__)
+
+# no me olvide de lo artesanal Croch, tranquilo
+def invertirString(txt):
+    resultado = ""
+    for char in txt:
+        resultado = char + resultado
+    
+    logger.info(f"Texto invertido: {resultado}")
+    return resultado
+
+@app.route('/invertirTexto', methods=['POST'])
+def ejecutarTarea():
+    data = request.get_json()
+    texto = data.get('texto', '')
+    resultado = invertirString(texto)
+    return jsonify({'resultado': resultado})
+
+@app.route('/health', methods=['GET'])
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+    
+    
