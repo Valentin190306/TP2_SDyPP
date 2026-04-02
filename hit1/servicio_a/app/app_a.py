65f077ddacc0477681ee1b8c30205620ae754870
GustavoContardi <lito.contardi@hotmail.com>
2026-04-01 23:19:17 -0300

    servicio_a inside

--- hit1/servicio_a/app/app_a.py
+++ hit1/servicio_a/app/app_a.py
@@ -1,1 +1,24 @@
-print("hola desde worker a")
+from flask import Flask, request, jsonify
+import logging
+import sys
+
+app = Flask(__name__)
+
+# no me olvide de lo artesanal Croch, tranquilo
+def invertirString(txt):
+    resultado = ""
+    for char in txt:
+        resultado = char + resultado
+    return resultado
+
+@app.route('/invertirTexto', methods=['POST'])
+def ejecutar_tarea():
+    data = request.get_json()
+    texto = data.get('texto', '')
+    resultado = invertirString(texto)
+    return jsonify({'resultado': resultado})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+    
+    
